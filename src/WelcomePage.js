a2811735bca0d1b4485e51d1a5c52e8b3a914018
V1127 <111186746+V1127@users.noreply.github.com>
2023-09-04 22:07:48 +0530

    Update WelcomePage.js

--- src/WelcomePage.js
+++ src/WelcomePage.js
@@ -1,12 +1,1 @@
-import React from 'react';
 
-const WelcomePage = () => {
-  return (
-    <div className="WelcomePage">
-      <h1>Welcome to Our Website!</h1>
-      <p>Congratulations on signing up. You are now a member of our community.</p>
-    </div>
-  );
-};
-
-export default WelcomePage;
